c2ad857199fe5e2497b5f66dd745ec2eb273182c
Zazh <pobiz@inbox.ru>
2025-06-19 08:05:56 +0500

    fix: SEO moments

--- decorkz/app/core/urls.py
+++ decorkz/app/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from products.views import catalog, product_detail, catalog_root
 from blog.views import home
@@ -16,8 +17,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls', namespace='products')),
     path('blog/', include('blog.urls')),
-    path('', include('contacts.urls')),  # Контакты
-
+    path('', include('contacts.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
